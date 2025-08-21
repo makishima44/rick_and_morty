c490c249cb001f118dcef92950a542ae9327dcda
Artur <metallist15431543@gmail.com>
2025-08-21 21:08:59 +0300

    feat: yse styled Pagination component on page

--- src/component/pagination/Pagination.tsx
+++ src/component/pagination/Pagination.tsx
@@ -1,4 +1,5 @@
 import { Button } from "../button";
+import s from "./Pagination.module.css";
 
 type PaginationProps = {
   totalCount: number;
@@ -7,7 +8,7 @@ type PaginationProps = {
 };
 
 export const Pagination = ({ totalCount, currentPage, setPageChange }: PaginationProps) => {
-  const visiblePages = 5;
+  const visiblePages = 3;
 
   let start = Math.max(1, currentPage - Math.floor(visiblePages / 2));
   let end = start + visiblePages - 1;
@@ -20,14 +21,14 @@ export const Pagination = ({ totalCount, currentPage, setPageChange }: Paginatio
   const pages = Array.from({ length: end - start + 1 }, (_, i) => start + i);
 
   return (
-    <div>
+    <div className={s.paginationBlock}>
       <Button onClick={() => setPageChange(currentPage - 1)} disabled={currentPage === 1}>
         Prev
       </Button>
 
       {pages.map((page) => {
         return (
-          <Button onClick={() => setPageChange(page)} key={page} variant={"pagination"}>
+          <Button onClick={() => setPageChange(page)} key={page} variant={"pagination"} className={page === currentPage ? s.activePage : ""}>
             {page}
           </Button>
         );
