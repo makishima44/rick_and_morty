53b4cd9d084b1ad9dfbd4f203eec6ff0d948220e
Artur <metallist15431543@gmail.com>
2025-08-21 17:54:28 +0300

    feat(button): add variant prop for pagination buttons

--- src/component/button/Button.tsx
+++ src/component/button/Button.tsx
@@ -1,11 +1,15 @@
 import type { ComponentPropsWithoutRef } from "react";
 import s from "./Button.module.css";
 
-type ButtonProps = ComponentPropsWithoutRef<"button">;
+type ButtonProps = ComponentPropsWithoutRef<"button"> & {
+  variant?: "default" | "pagination";
+};
 
-export const Button = ({ children, ...rest }: ButtonProps) => {
+export const Button = ({ children, variant = "default", ...rest }: ButtonProps) => {
+  const classes = `${s.button} ${variant === "pagination" ? s["button-pagination"] : ""}`;
+
   return (
-    <button className={s.button} {...rest}>
+    <button className={classes} {...rest}>
       <p>{children}</p>
     </button>
   );
