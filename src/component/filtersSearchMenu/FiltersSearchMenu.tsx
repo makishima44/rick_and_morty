7731891e79bbb2bcea763dc063c9e9f7c449a122
Artur <metallist15431543@gmail.com>
2025-08-30 12:51:22 +0300

    refactor: rename FIlter to FIlterSearchMenu

--- src/component/filtersSearchMenu/FiltersSearchMenu.tsx
+++ src/component/filtersSearchMenu/FiltersSearchMenu.tsx
@@ -1,6 +1,7 @@
 import { useGetAllCharactersQuery } from "../../services/rickAndMortyApi";
-import { Dropdown } from "../dropdown/Dropdown";
-import s from "./Filtets.module.css";
+import { Dropdown } from "../dropdown";
+
+import s from "./FiltersSearchMenu.module.css";
 
 type FiltersProps = {
   onStatusChange: (status: string) => void;
@@ -8,19 +9,18 @@ type FiltersProps = {
   onGenderChange: (status: string) => void;
 };
 
-export const Filters = ({ onStatusChange, onSpeciesChange, onGenderChange }: FiltersProps) => {
+export const FiltersSearchMenu = ({ onStatusChange, onSpeciesChange, onGenderChange }: FiltersProps) => {
   const { data } = useGetAllCharactersQuery({ page: 1, status: null, species: null, gender: null });
 
   const statusOptions = [...new Set(data?.results.map((c) => c.status))];
   const speciesOptions = [...new Set(data?.results.map((c) => c.species))];
   const genderOptions = [...new Set(data?.results.map((c) => c.gender))];
-  console.log(speciesOptions);
 
   return (
     <div className={s.filtersBlock}>
       <Dropdown options={statusOptions} onSelect={onStatusChange} placeholder='select status' />
-      <Dropdown options={speciesOptions} onSelect={onSpeciesChange} placeholder='select status' />
-      <Dropdown options={genderOptions} onSelect={onGenderChange} placeholder='select status' />
+      <Dropdown options={speciesOptions} onSelect={onSpeciesChange} placeholder='select species' />
+      <Dropdown options={genderOptions} onSelect={onGenderChange} placeholder='select gender' />
     </div>
   );
 };
